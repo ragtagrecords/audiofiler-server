50ae8df281e835a01767d798328e8563fdc23664
Michael Jarvis <mjarvis1997@gmail.com>
2022-04-17 20:56:55 -0400

    adjusted dir API and started working with database

--- app.js
+++ app.js
@@ -1,75 +1,13 @@
-// http://192.168.86.163:3000/
-
 var http = require('http');
 var express = require('express');
-
-//var path = require('path');
 var PORT = 3000;
 var app = express();
 var public = require('./public.js');
+const cors = require('cors');
+
+app.use(cors());
+
 app.use('/public', public);
-
-
-
-
-/*
-// Without middleware
-router.get('/', function(req, res){
-    var options = {
-        root: path.join(__dirname)
-    };
-    
-     
-    var fileName = 'sample1.mp3';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.log("req: ");
-            console.log(req);
-            console.log("path: ");
-            console.log(path);
-            console.log("dirname: ");
-            console.log(__dirname);
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-            console.log("req: ");
-            console.log(req);
-            console.log("path: ");
-            console.log(path);
-            console.log("dirname: ");
-            console.log(__dirname);
-        }
-    });
-});
-
-router.get('/public/audio/:fileName', function(req, res){
-    var options = {
-        root: path.join(__dirname)
-    };
-    console.log(options.root);
-     
-    var fileName = req.params.fileName;
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.log("req params: ");
-            console.log(req.params);
-            console.log("path: ");
-            console.log(path);
-            console.log("dirname: ");
-            console.log(__dirname);
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-            console.log("req params: ");
-            console.log(req.params);
-            console.log("path: ");
-            console.log(path);
-            console.log("dirname: ");
-            console.log(__dirname);
-        }
-    });
-});
-*/
 
 app.listen(PORT, function(err){
     if (err) console.log(err);
--- public.js
+++ public.js
@@ -1,60 +1,93 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-var http = require('http');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const mysql = require('mysql');
 
-// Home page route.
+
+// handles JSON response for GET requests to a directory
+function sendListOfFilesInFolder(req, res, folderPath) {
+  dirPath = path.join(__dirname) + '/public' + folderPath;
+
+  options = {
+    withFileTypes: true
+  }
+
+  var fileNames = fs.readdirSync(dirPath, options);
+  response = { };
+
+  // TODO: use actual ID from database, not i
+  for (let i = 0; i < fileNames.length; ++i) {
+    response[i] = fileNames[i].name;
+  }
+
+  res.send(response);
+
+}
+
+function sendFileAsResponse(req, res, dirName) {
+  var options = {
+    root: path.join(__dirname) + '/public/' + dirName + '/'
+  };
+  
+  var fileName = req.params.fileName;
+  res.sendFile(fileName, options, function (err) {
+      if (err) {
+          console.log(err);
+      } else {
+          console.log('Sent:', fileName);
+      }
+  });
+}
+
 router.get('/', function (req, res) {
   res.send('public root');
 })
-  
-// TODO: make this return a list of all the audio filenames in the folder
+
+// return list of all files in directory, enumerated by ID
 router.get('/audio', function (req, res) {
-  res.send('audio folder');
+  // db
+  const db = mysql.createConnection({
+    user: "audiofiler-fs",
+    host: "150.238.75.231",
+    password: "weloveclouddatabasesolutions",
+    database: "audiofiler"
+  });
+
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  db.query(
+    `INSERT INTO audios (path, name, tempo) VALUES (?,?,?)`,
+    ['sample1.mp3', null, 111],
+    (err, result) => {
+      if (err) {
+          console.log(err.sqlMessage);
+      } else {
+        console.log(result);
+      }
+    }
+  );
+
+
+  sendListOfFilesInFolder(req, res, '/audio');
 })
 
-
-// get audio file by name
+// About page route.
 router.get('/audio/:fileName', function (req, res) {
-
-  var options = {
-    root: path.join(__dirname) + '/public/audio/'
-  };
-  console.log("options.root: ");
-  console.log(options.root);
-  
-  var fileName = req.params.fileName;
-  res.sendFile(fileName, options, function (err) {
-      if (err) {
-          next(err);
-      } else {
-          console.log('Sent:', fileName);
-      }
-  });
+  sendFileAsResponse(req, res, 'audio')
 })
 
-// TODO: make this return a list of all the image filenames in the folder
-router.get('/iamge', function (req, res) {
-  res.send('image folder');
+// return list of all files in directory, enumerated by ID
+router.get('/images', function (req, res) {
+  sendListOfFilesInFolder(req, res, '/images');
 })
 
-// get image file by name
 router.get('/images/:fileName', function (req, res) {
-
-  var options = {
-    root: path.join(__dirname) + '/public/images/'
-  };
-  console.log("options.root: ");
-  console.log(options.root);
-  
-  var fileName = req.params.fileName;
-  res.sendFile(fileName, options, function (err) {
-      if (err) {
-          next(err);
-      } else {
-          console.log('Sent:', fileName);
-      }
-  });
+  sendFileAsResponse(req, res, 'images');
 })
 
 // necessary with express.Router()
